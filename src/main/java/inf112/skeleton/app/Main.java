e8257c55f3b8c5f83790dc7059a7209c2153dd8e
Kim André Austgulen <Kim.Austgulen@student.uib.no>
2020-01-30 14:32:01 +0100

    TEST COMMIT KIM

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -3,7 +3,7 @@ package inf112.skeleton.app;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
-
+//TEST KIM
 public class Main {
     public static void main(String[] args) {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
