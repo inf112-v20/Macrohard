f512f8e4bd282d849a28693901ae064d412f4e5e
baa010 <Kr0k@nisen>
2020-01-30 15:27:42 +0100

    removed test-pulls

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -3,9 +3,6 @@ package inf112.skeleton.app;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
-//TEST KIM
-// TEST OSCAR
-// Test Bendik
 public class Main {
     public static void main(String[] args) {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
