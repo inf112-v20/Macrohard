705136d3761d2cef6fed0c94cb38313f5a226e4c
baa010 <Kr0k@nisen>
2020-01-31 14:31:47 +0100

    method for initializing board as 2d boolean array

--- src/main/java/inf112/skeleton/app/Board.java
+++ src/main/java/inf112/skeleton/app/Board.java
@@ -1,16 +1,28 @@
 package inf112.skeleton.app;
 
+import java.util.Arrays;
+
 public class Board {
 
-    private final int heigth;
+    private final int height;
     private final int width;
 
     private Boolean[][] board;
     private Player player;
 
-    public Board(int heigth, int width, Boolean[][] board) {
-        this.heigth = heigth;
+    public Board(int height, int width) {
+        this.height = height;
         this.width = width;
-        this.board = board;
+
+        board = initializeBoard(height, width);
     }
+
+    public Boolean[][] initializeBoard(int height, int width) {
+        Boolean[][] board = new Boolean[height][width];
+        for (Boolean[] b : board) {
+            Arrays.fill(b, false);
+        }
+        return board;
+    }
+
 }
--- src/main/java/inf112/skeleton/app/Player.java
+++ src/main/java/inf112/skeleton/app/Player.java
@@ -1,11 +1,20 @@
 package inf112.skeleton.app;
 
 public class Player {
+
     private int x;
     private int y;
 
     public Player(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 }
