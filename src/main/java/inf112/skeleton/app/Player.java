935c8b81efaaad5f1d634355adadb7458d861cce
Kim André Austgulen <Kim.Austgulen@student.uib.no>
2020-01-31 13:48:53 +0100

    Create Player class with constructor

---
+++ src/main/java/inf112/skeleton/app/Player.java
@@ -0,0 +1,11 @@
+package inf112.skeleton.app;
+
+public class Player {
+    private int x;
+    private int y;
+
+    public Player(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
