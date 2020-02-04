a622bb066546b928421ea0369d247c239c7477d9
baa010 <Kr0k@nisen>
2020-02-04 19:53:43 +0100

    Renamed HelloWorld to BoardGraphic

--- src/main/java/inf112/skeleton/app/BoardGraphic.java
+++ src/main/java/inf112/skeleton/app/BoardGraphic.java
@@ -12,7 +12,7 @@ import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
-public class HelloWorld implements ApplicationListener {
+public class BoardGraphic implements ApplicationListener {
     private SpriteBatch batch;
     private BitmapFont font;
     private TiledMap map;
