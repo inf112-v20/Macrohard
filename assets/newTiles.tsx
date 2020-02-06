caef5642b15af487d076d4d7aa56a98fc9650d7b
Kim André Austgulen <Kim.Austgulen@student.uib.no>
2020-02-06 15:37:39 +0100

    clean assets folder

--- assets/newTiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="newTiles" tilewidth="75" tileheight="75" tilecount="2176" columns="32">
- <image source="perfectTiles.png" width="2400" height="5100"/>
-</tileset>
