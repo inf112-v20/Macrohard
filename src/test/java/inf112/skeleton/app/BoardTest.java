63484f67689db395ce9bb394568e01f91a00b0c8
Oscar Sommervold <Oscar.Sommervold@student.uib.no>
2020-02-07 10:59:49 +0100

    Test cleaning

--- src/test/java/inf112/skeleton/app/BoardTest.java
+++ src/test/java/inf112/skeleton/app/BoardTest.java
@@ -7,36 +7,35 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class BoardTest {
+    Player player;
+    Board board;
 
     @Before
-    public void setUp() throws Exception {
-
+    public void setUp() {
+        player = new Player(3,2);
+        board = new Board(player,10, 10);
     }
 
     @Test
     public void playerMustBePlacedOnBoard() {
         Player player = new Player(11, 11);
         Board board = new Board(player,10, 10);
-
         Assert.assertNull(board.getPlayer());
     }
 
     @Test
     public void playerTileIsOccupied() {
-        Player player = new Player(3, 2);
-        Board board = new Board(player, 5, 5);
-
         assertTrue(board.getTile(3,2).isOccupied());
+        assertFalse(board.getTile(4,2).isOccupied());
     }
 
     @Test
     public void updatedBoardHasCorrectValues() {
-        Player player = new Player(3, 2);
-        Board board = new Board(player,10, 10);
         board.update(3,2,0);
-        board.update(3,3,1);
         assertEquals(board.getTile(3,2).isOccupied(),false);
+        board.update(3,3,1);
         assertEquals(board.getTile(3,3).isOccupied() , true);
     }
+
 
 }
