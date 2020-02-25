a1647aa2b0742a8bfc68e0f980b2227f56c01b15
Vegard Nerhus <vegard@nerhus.no>
2020-02-25 18:18:06 +0100

    Added preferences and a PreferenceScreen.

--- src/main/java/inf112/skeleton/app/Roborally.java
+++ src/main/java/inf112/skeleton/app/Roborally.java
@@ -1,15 +1,21 @@
 package inf112.skeleton.app;
 
 
+import com.badlogic.gdx.Preferences;
+import inf112.skeleton.app.preferences.AppPreferences;
 import inf112.skeleton.app.screens.LoadingScreen;
 import inf112.skeleton.app.screens.MainScreen;
 import inf112.skeleton.app.screens.MenuScreen;
+import inf112.skeleton.app.screens.PreferenceScreen;
 
 public class Roborally extends com.badlogic.gdx.Game {
 
     private LoadingScreen loadingScreen;
     private MenuScreen menuScreen;
     private MainScreen mainScreen;
+    private PreferenceScreen preferenceScreen;
+
+    private AppPreferences appPreferences;
 
     public final static int MENU = 0;
     public final static int PREFERENCES = 1;
@@ -19,6 +25,11 @@ public class Roborally extends com.badlogic.gdx.Game {
     public void create() {
         loadingScreen = new LoadingScreen(this);
         setScreen(loadingScreen);
+        appPreferences = new AppPreferences();
+    }
+
+    public AppPreferences getPreferences(){
+        return appPreferences;
     }
 
     public void changeScreen(int screen){
@@ -30,7 +41,13 @@ public class Roborally extends com.badlogic.gdx.Game {
                 this.setScreen(menuScreen);
                 menuScreen.setAsInputProcessor();
                 break;
-            case PREFERENCES: break;
+            case PREFERENCES:
+                if(preferenceScreen == null) {
+                    preferenceScreen = new PreferenceScreen(this);
+                }
+                this.setScreen(preferenceScreen);
+                preferenceScreen.setAsInputProcessor();
+                break;
             case APPLICATION:
                 if(mainScreen == null){
                     mainScreen = new MainScreen(this);
