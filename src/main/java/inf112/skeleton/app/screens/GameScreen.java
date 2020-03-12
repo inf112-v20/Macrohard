0b5416696198aacf19a009c66118705869bcfc60
baa010 <Kr0k@nisen>
2020-03-12 15:01:58 +0100

    #40 Refactored GameScreen-class to support multiple Players/PlayerGraphics, some additional cleaning too

--- src/main/java/inf112/skeleton/app/screens/GameScreen.java
+++ src/main/java/inf112/skeleton/app/screens/GameScreen.java
@@ -9,13 +9,9 @@ import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.actions.Actions;
-import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.utils.Timer;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import inf112.skeleton.app.*;
 import inf112.skeleton.app.cards.*;
 import inf112.skeleton.app.graphics.CardGraphic;
@@ -23,7 +19,8 @@ import inf112.skeleton.app.graphics.PlayerGraphic;
 import inf112.skeleton.app.managers.MainScreenInputManager;
 import inf112.skeleton.app.managers.TiledMapManager;
 
-import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class GameScreen implements Screen {
 
@@ -41,6 +38,8 @@ public class GameScreen implements Screen {
     private TiledMapTileLayer boardLayer;
     private TiledMapTileLayer playerLayer;
 
+    private ArrayList<PlayerGraphic> playerGraphics = new ArrayList<>();
+
     private Board board;
 
     private Stage stage;
@@ -50,39 +49,15 @@ public class GameScreen implements Screen {
         stage = new Stage(new ScreenViewport());
 
         this.parent = parent;
-        final Player player = new Player(1,1, Direction.NORTH);
-        board = new Board(player, 12,12);
-        //map = new TmxMapLoader().load("assets/robomap.tmx");
-
-        //TESTING
+        Player player1 = new Player(1,1, Direction.NORTH);
+        Player player2 = new Player(9, 9, Direction.SOUTH);
+        final ArrayList<Player> players = (ArrayList<Player>) Arrays.asList(player1, player2);
+        board = new Board(players, 12,12);
 
         TiledMapManager handler = new TiledMapManager("assets/plsWork.tmx");
-        TiledMapTileLayer.Cell cell = handler.getCell(7,3,"FLOOR");
-        TiledMapTileLayer.Cell cell1 = handler.getCell(1,0,0);
-        TiledMapTileLayer.Cell cell3 = handler.getCell(2,0,0);
-        TiledMapTileLayer.Cell cell4 = handler.getCell(3,0,0);
-        TiledMapTileLayer.Cell cell5 = handler.getCell(4,0,0);
-        TiledMapTileLayer.Cell cell6 = handler.getCell(5,0,0);
-        TiledMapTileLayer.Cell cell7 = handler.getCell(6,0,0);
 
-        System.out.println(Tile.getType(cell1));
-        System.out.println(Tile.getType(cell3));
-        System.out.println(Tile.getType(cell4));
-        System.out.println(Tile.getType(cell5));
-        System.out.println(Tile.getType(cell6));
-        System.out.println(Tile.getType(cell7));
-
-
-        System.out.println(Tile.getType(cell));
-        //System.out.println(TileType.getType(cell2));
-        TiledMapTileLayer floor = (TiledMapTileLayer) handler.getMap().getLayers().get("Player");
-        System.out.println(floor.getCell(5,5) != null);
-        //System.out.println(cell.setTile());
-        System.out.println(handler.getMap().getLayers().get(0).getName());
-        //System.out.println(handler.getTile(0,0,"FLOOR").getTileType());
         map = handler.getMap();
 
-        //
         camera = new OrthographicCamera();
         camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         camera.translate(0, -tileSize*camera.zoom*2);
@@ -91,47 +66,41 @@ public class GameScreen implements Screen {
 
         playerLayer = (TiledMapTileLayer) map.getLayers().get("Player");
 
-        TiledMapTileLayer.Cell playerCell = new TiledMapTileLayer.Cell();
+        Deck deck = new Deck();
+        deck.shuffle();
 
-        playerLayer.setCell(player.getRow(), player.getCol(), playerCell);
+        for (Player player : players) {
+            deck.dealHand(player);
+            TiledMapTileLayer.Cell playerCell = new TiledMapTileLayer.Cell();
+            playerLayer.setCell(player.getRow(), player.getCol(), playerCell);
+            PlayerGraphic playerGraphic = new PlayerGraphic(player);
+            playerGraphics.add(playerGraphic);
+            stage.addActor(playerGraphic);
+        }
 
         renderer = new OrthogonalTiledMapRenderer(map);
 
-        Deck deck = new Deck();
-        deck.shuffle();
-        deck.dealHand(player);
-        PlayerHand hand = player.getHand();
+        PlayerHand hand = players.get(0).getHand();
         Card[] cards = hand.getPossibleHand();
-        //Print-line for testing purposes
 
         for (Card card : cards) {
             CardGraphic tempCard = new CardGraphic(card);
             stage.addActor(tempCard);
         }
- /*      player.setFinalHand();
-        int cardIndex = 1;
-        for (int j = 0; j<cards.length; j++) {
-            if (cards[j].handIndex == cardIndex) {
-                player.getFinalHand()[cardIndex-1] = cards[j];
-                cardIndex++;
-            }
-        }
-        */
-
-        PlayerGraphic playerGraphic = new PlayerGraphic(player);
-        stage.addActor(playerGraphic);
 
         TextButton button = new TextButton("GO!", parent.getSkin());
         button.setBounds(750, 200, 100, 50);
         button.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent changeEvent, Actor actor) {
-                runProgram(player);
+                for (Player player : players) {
+                    runProgram(player);
+                }
             }
         });
         stage.addActor(button);
 
-        ip = new MainScreenInputManager(parent, boardLayer, playerLayer, playerCell, player, board);
+        ip = new MainScreenInputManager(parent);
     }
 
     public void runProgram(Player player) {
