d381562d8caec4e2465a598a5553998900cd7315
Henrik <hki006@uib.no>
2020-03-19 16:02:48 +0100

    #41

--- assets/tileset.tsx
+++ assets/tileset.tsx
@@ -1,6 +1,30 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="tileset" tilewidth="60" tileheight="60" tilecount="210" columns="8">
+<tileset version="1.2" tiledversion="1.3.2" name="tileset" tilewidth="60" tileheight="60" tilecount="136" columns="8">
  <image source="tiles60.png" width="480" height="1020"/>
+ <tile id="0">
+  <properties>
+   <property name="Direction" value=""/>
+   <property name="Type" value=""/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="Direction" value=""/>
+   <property name="Type" value=""/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="Direction" value=""/>
+   <property name="Type" value=""/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="Direction" value=""/>
+   <property name="Type" value=""/>
+  </properties>
+ </tile>
  <tile id="4">
   <properties>
    <property name="Type" value="FLOOR"/>
@@ -8,52 +32,554 @@
  </tile>
  <tile id="5" type="HOLE">
   <properties>
+   <property name="Damage" value="10"/>
    <property name="Type" value="HOLE"/>
   </properties>
  </tile>
  <tile id="6">
   <properties>
+   <property name="Remove_damage" value="2"/>
    <property name="Type" value="REPAIR"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="Direction" value="SOUTH_EAST"/>
+   <property name="Type" value="WALL"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="Direction" value=""/>
+   <property name="Type" value=""/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="Direction" value=""/>
+   <property name="Type" value=""/>
   </properties>
  </tile>
  <tile id="10">
   <properties>
-   <property name="Type" value="CONVEYOR_EXPRESS_NORTH"/>
+   <property name="Direction" value=""/>
+   <property name="Type" value=""/>
   </properties>
  </tile>
  <tile id="11">
   <properties>
-   <property name="Type" value="CONVEYOR_EXPRESS_EAST"/>
+   <property name="Direction" value=""/>
+   <property name="Type" value=""/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="Direction" value="NORTH"/>
+   <property name="Type" value="CONVEYOR_EXPRESS"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="Direction" value="EAST"/>
+   <property name="Type" value="CONVEYOR_EXPRESS"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="Remove_damage" value="1"/>
+   <property name="Type" value="REPAIR"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="Direction" value="NORTH_EAST"/>
+   <property name="Type" value="WALL"/>
   </properties>
  </tile>
  <tile id="16">
   <properties>
-   <property name="Type" value="CONVEYOR_EXPRESS_SOUTH"/>
+   <property name="Direction" value="WEST_SOUTH"/>
+   <property name="Type" value="CONVEYOR_EXPRESS"/>
   </properties>
  </tile>
  <tile id="17">
   <properties>
-   <property name="Type" value="CONVEYOR_EXPRESS_WEST"/>
+   <property name="Direction" value="NORTH_WEST"/>
+   <property name="Type" value="CONVEYOR_EXPRESS"/>
   </properties>
  </tile>
  <tile id="18">
   <properties>
-   <property name="Type" value="WALL_EAST"/>
+   <property name="Direction" value="NORTH_EAST"/>
+   <property name="Type" value="CONVEYOR_EXPRESS"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="Direction" value="EAST_SOUTH"/>
+   <property name="Type" value="CONVEYOR_EXPRESS"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="Direction" value="SOUTH"/>
+   <property name="Type" value="CONVEYOR_EXPRESS"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="Direction" value="WEST"/>
+   <property name="Type" value="CONVEYOR_EXPRESS"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="Direction" value="EAST"/>
+   <property name="Type" value="WALL"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="Direction" value="NORTH_WEST"/>
+   <property name="Type" value="WALL"/>
   </properties>
  </tile>
  <tile id="24">
   <properties>
-   <property name="Type" value="WALL_NORTH"/>
+   <property name="Direction" value="SOUTH_EAST"/>
+   <property name="Type" value="CONVEYOR_EXPRESS"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="Direction" value="EAST_NORTH"/>
+   <property name="Type" value="CONVEYOR_EXPRESS"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="Direction" value=""/>
+   <property name="Type" value="CONVEYOR_EXPRESS"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="Direction" value="SOUTH_WEST"/>
+   <property name="Type" value="CONVEYOR_EXPRESS"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="Direction" value="SOUTH"/>
+   <property name="Type" value="WALL"/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="Direction" value="WEST"/>
+   <property name="Type" value="WALL"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="Direction" value="NORTH"/>
+   <property name="Type" value="WALL"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="Direction" value="SOUTH_WEST"/>
+   <property name="Type" value="WALL"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="Direction" value="WEST_SOUTH"/>
+   <property name="Type" value="CONVEYOR_BELT"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="Direction" value="SOUTH_WEST"/>
+   <property name="Type" value="CONVEYOR_BELT"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="Direction" value="NORTH_EAST"/>
+   <property name="Type" value="CONVEYOR_BELT"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="Direction" value="WEST_SOUTH"/>
+   <property name="Type" value="CONVEYOR_BELT"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="Direction" value="SOUTH"/>
+   <property name="Type" value="WALL_LASER"/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="Direction" value="WEST"/>
+   <property name="Type" value="WALL_LASER"/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="Damage" value="1"/>
+   <property name="Direction" value="HORIZONTAL"/>
+   <property name="Type" value="LASER"/>
+  </properties>
+ </tile>
+ <tile id="39">
+  <properties>
+   <property name="Damage" value="1"/>
+   <property name="Direction" value="CROSSING"/>
+   <property name="Type" value="LASER"/>
+  </properties>
+ </tile>
+ <tile id="40">
+  <properties>
+   <property name="Direction" value="SOUTH_EAST"/>
+   <property name="Type" value="CONVEYOR_BELT"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="Direction" value="WEST_NORTH"/>
+   <property name="Type" value="CONVEYOR_BELT"/>
   </properties>
  </tile>
  <tile id="42">
   <properties>
-   <property name="Type" value="GEAR_COUNTERCLOCK"/>
+   <property name="Direction" value="WEST_NORTH"/>
+   <property name="Type" value="CONVEYOR_BELT"/>
   </properties>
  </tile>
  <tile id="43">
   <properties>
-   <property name="Type" value="GEAR_CLOCK"/>
+   <property name="Direction" value="SOUTH_WEST"/>
+   <property name="Type" value="CONVEYOR_BELT"/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="Direction" value="NORTH"/>
+   <property name="Type" value="WALL_LASER"/>
+  </properties>
+ </tile>
+ <tile id="45">
+  <properties>
+   <property name="Direction" value="EAST"/>
+   <property name="Type" value="WALL_LASER"/>
+  </properties>
+ </tile>
+ <tile id="46">
+  <properties>
+   <property name="Damage" value="1"/>
+   <property name="Direction" value="VERTICAL"/>
+   <property name="Type" value="LASER"/>
+  </properties>
+ </tile>
+ <tile id="48">
+  <properties>
+   <property name="Direction" value="NORTH"/>
+   <property name="Type" value="CONVEYOR_BELT"/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="Direction" value="SOUTH"/>
+   <property name="Type" value="CONVEYOR_BELT"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="Direction" value="WEST"/>
+   <property name="Type" value="CONVEYOR_BELT"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="Direction" value="EAST"/>
+   <property name="Type" value="CONVEYOR_BELT"/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <properties>
+   <property name="Turn" value="COUNTER_CLOCKWISE"/>
+   <property name="Type" value="GEAR"/>
+  </properties>
+ </tile>
+ <tile id="53">
+  <properties>
+   <property name="Turn" value="CLOCKWISE"/>
+   <property name="Type" value="GEAR"/>
+  </properties>
+ </tile>
+ <tile id="54">
+  <properties>
+   <property name="Number" value="1"/>
+   <property name="Type" value="CHECKPOINT"/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="Direction" value="NORTH/EAST_NORTH"/>
+   <property name="Type" value="CONVEYOR_BELT"/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="Direction" value="EAST/SOUTH_EAST"/>
+   <property name="Type" value="CONVEYOR_BELT"/>
+  </properties>
+ </tile>
+ <tile id="58">
+  <properties>
+   <property name="Direction" value="SOUTH/WEST_SOUTH"/>
+   <property name="Type" value="CONVEYOR_BELT"/>
+  </properties>
+ </tile>
+ <tile id="59">
+  <properties>
+   <property name="Direction" value="WEST/NORTH_WEST"/>
+   <property name="Type" value="CONVEYOR_BELT"/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="Direction" value="NORTH_EAST/SOUTH_EAST"/>
+   <property name="Type" value="CONVEYOR_BELT"/>
+  </properties>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="Direction" value="WEST_SOUTH/EAST_SOUTH"/>
+   <property name="Type" value="CONVEYOR_BELT"/>
+  </properties>
+ </tile>
+ <tile id="62">
+  <properties>
+   <property name="Number" value="2"/>
+   <property name="Type" value="CHECKPOINT"/>
+  </properties>
+ </tile>
+ <tile id="64">
+  <properties>
+   <property name="Direction" value="NORTH/WEST_NORTH"/>
+   <property name="Type" value="CONVEYOR_BELT"/>
+  </properties>
+ </tile>
+ <tile id="65">
+  <properties>
+   <property name="Direction" value="EAST/NORTH_EAST"/>
+   <property name="Type" value="CONVEYOR_BELT"/>
+  </properties>
+ </tile>
+ <tile id="66">
+  <properties>
+   <property name="Direction" value="SOUTH/EAST_SOUTH"/>
+   <property name="Type" value="CONVEYOR_BELT"/>
+  </properties>
+ </tile>
+ <tile id="67">
+  <properties>
+   <property name="Direction" value="WEST/SOUTH_WEST"/>
+   <property name="Type" value="CONVEYOR_BELT"/>
+  </properties>
+ </tile>
+ <tile id="68">
+  <properties>
+   <property name="Direction" value="WEST_NORTH/EAST_NORTH"/>
+   <property name="Type" value="CONVEYOR_BELT"/>
+  </properties>
+ </tile>
+ <tile id="69">
+  <properties>
+   <property name="Direction" value="NORTH_WEST/SOUTH_WEST"/>
+   <property name="Type" value="CONVEYOR_BELT"/>
+  </properties>
+ </tile>
+ <tile id="70">
+  <properties>
+   <property name="Number" value="3"/>
+   <property name="Type" value="CHECKPOINT"/>
+  </properties>
+ </tile>
+ <tile id="72">
+  <properties>
+   <property name="Direction" value="NORTH/EAST_NORTH"/>
+   <property name="Type" value="CONVEYOR_EXPRESS"/>
+  </properties>
+ </tile>
+ <tile id="73">
+  <properties>
+   <property name="Direction" value="EAST/SOUTH_EAST"/>
+   <property name="Type" value="CONVEYOR_EXPRESS"/>
+  </properties>
+ </tile>
+ <tile id="74">
+  <properties>
+   <property name="Direction" value="SOUTH/WEST_SOUTH"/>
+   <property name="Type" value="CONVEYOR_EXPRESS"/>
+  </properties>
+ </tile>
+ <tile id="75">
+  <properties>
+   <property name="Direction" value="WEST/NORTH_WEST"/>
+   <property name="Type" value="CONVEYOR_EXPRESS"/>
+  </properties>
+ </tile>
+ <tile id="76">
+  <properties>
+   <property name="Direction" value="NORTH/WEST_NORTH"/>
+   <property name="Type" value="CONVEYOR_EXPRESS"/>
+  </properties>
+ </tile>
+ <tile id="77">
+  <properties>
+   <property name="Direction" value="EAST/NORTH_EAST"/>
+   <property name="Type" value="CONVEYOR_EXPRESS"/>
+  </properties>
+ </tile>
+ <tile id="78">
+  <properties>
+   <property name="Number" value="4"/>
+   <property name="Type" value="CHECKPOINT"/>
+  </properties>
+ </tile>
+ <tile id="80">
+  <properties>
+   <property name="Direction" value="NORTH_EAST/SOUTH_EAST"/>
+   <property name="Type" value="CONVEYOR_EXPRESS"/>
+  </properties>
+ </tile>
+ <tile id="81">
+  <properties>
+   <property name="Direction" value="WEST_SOUTH/EAST_SOUTH"/>
+   <property name="Type" value="CONVEYOR_EXPRESS"/>
+  </properties>
+ </tile>
+ <tile id="82">
+  <properties>
+   <property name="Direction" value="NORTH_WEST/SOUTH_WEST"/>
+   <property name="Type" value="CONVEYOR_EXPRESS"/>
+  </properties>
+ </tile>
+ <tile id="83">
+  <properties>
+   <property name="Direction" value="WEST_NORTH/EAST_NORTH"/>
+   <property name="Type" value="CONVEYOR_EXPRESS"/>
+  </properties>
+ </tile>
+ <tile id="84">
+  <properties>
+   <property name="Direction" value="WEST/SOUTH_WEST"/>
+   <property name="Type" value="CONVEYOR_EXPRESS"/>
+  </properties>
+ </tile>
+ <tile id="85">
+  <properties>
+   <property name="Direction" value="SOUTH/EAST_SOUTH"/>
+   <property name="Type" value="CONVEYOR_EXPRESS"/>
+  </properties>
+ </tile>
+ <tile id="86">
+  <properties>
+   <property name="Direction" value="SOUTH"/>
+   <property name="Type" value="WALL_LASER_DOUBLE"/>
+  </properties>
+ </tile>
+ <tile id="92">
+  <properties>
+   <property name="Direction" value="WEST"/>
+   <property name="Type" value="WALL_LASER_DOUBLE"/>
+  </properties>
+ </tile>
+ <tile id="93">
+  <properties>
+   <property name="Direction" value="NORTH"/>
+   <property name="Type" value="WALL_LASER_DOUBLE"/>
+  </properties>
+ </tile>
+ <tile id="94">
+  <properties>
+   <property name="Direction" value="EAST"/>
+   <property name="Type" value="WALL_LASER_DOUBLE"/>
+  </properties>
+ </tile>
+ <tile id="100">
+  <properties>
+   <property name="Damage" value="2"/>
+   <property name="Direction" value="CROSSING"/>
+   <property name="Type" value="LASER_DOUBLE"/>
+  </properties>
+ </tile>
+ <tile id="101">
+  <properties>
+   <property name="Damage" value="2"/>
+   <property name="Direction" value="VERTICAL"/>
+   <property name="Type" value="LASER_DOUBLE"/>
+  </properties>
+ </tile>
+ <tile id="102">
+  <properties>
+   <property name="Damage" value="2"/>
+   <property name="Direction" value="HORIZONTAL"/>
+   <property name="Type" value="LASER_DOUBLE"/>
+  </properties>
+ </tile>
+ <tile id="120">
+  <properties>
+   <property name="Number" value="1"/>
+   <property name="Type" value="START"/>
+  </properties>
+ </tile>
+ <tile id="121">
+  <properties>
+   <property name="Number" value="2"/>
+   <property name="Type" value="START"/>
+  </properties>
+ </tile>
+ <tile id="122">
+  <properties>
+   <property name="Number" value="3"/>
+   <property name="Type" value="START"/>
+  </properties>
+ </tile>
+ <tile id="123">
+  <properties>
+   <property name="Number" value="4"/>
+   <property name="Type" value="START"/>
+  </properties>
+ </tile>
+ <tile id="128">
+  <properties>
+   <property name="Number" value="5"/>
+   <property name="Type" value="START"/>
+  </properties>
+ </tile>
+ <tile id="129">
+  <properties>
+   <property name="Number" value="6"/>
+   <property name="Type" value="START"/>
+  </properties>
+ </tile>
+ <tile id="130">
+  <properties>
+   <property name="Number" value="7"/>
+   <property name="Type" value="START"/>
+  </properties>
+ </tile>
+ <tile id="131">
+  <properties>
+   <property name="Number" value="8"/>
+   <property name="Type" value="START"/>
   </properties>
  </tile>
 </tileset>
