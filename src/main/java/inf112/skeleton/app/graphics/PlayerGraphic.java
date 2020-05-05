24af8e2e83ec1254300598fa00f92ea2f28feece
baa010 <Kr0k@nisen>
2020-05-05 11:54:57 +0200

    refactored PlayerGraphic and made flags repair damaged robots when touched

--- src/main/java/inf112/skeleton/app/graphics/PlayerGraphic.java
+++ src/main/java/inf112/skeleton/app/graphics/PlayerGraphic.java
@@ -19,10 +19,12 @@ public class PlayerGraphic extends Image {
     private final float TILE_SIZE = GameScreen.TILE_SIZE;
 
     private final Player player;
+    private final String color;
 
     public boolean isVisible = true;
 
     // ANIMATION
+    private static final String[] COLORS = new String[]{"", "_BLACK", "_GREEN", "_PURPLE", "_WHITE", "_YELLOW", "_BLUE2", "", "", "", ""};
     private static final int HORIZONTAL_FRAME_COLS = 8;
     private static final  int HORIZONTAL_FRAME_ROWS = 1;
     private static final int BACK_FRAME_COLS = 8;
@@ -48,45 +50,10 @@ public class PlayerGraphic extends Image {
         staticTimeModifier += 0.30f;
         timeModifier = staticTimeModifier;
 
-        String[] colors = {"", "_BLACK", "_GREEN", "_PURPLE", "_WHITE", "_YELLOW", "_BLUE2", "", "", "", ""};
-        String colorModifier = colors[player.name()];
-
-        Texture idleHorizontalSheet = new Texture(Gdx.files.internal("./assets/sprites/robotSide" + colorModifier + ".png"));
-        TextureRegion[][] idleHorizontalTR = TextureRegion.split(idleHorizontalSheet,
-                idleHorizontalSheet.getWidth() / HORIZONTAL_FRAME_COLS,
-                idleHorizontalSheet.getHeight() / HORIZONTAL_FRAME_ROWS);
-        TextureRegion[] idleHorizontalFrames = new TextureRegion[HORIZONTAL_FRAME_COLS * HORIZONTAL_FRAME_ROWS];
-        int index = 0;
-        for (int i = 0; i < HORIZONTAL_FRAME_ROWS; i++) {
-            for (int j = 0; j < HORIZONTAL_FRAME_COLS; j++) {
-                idleHorizontalFrames[index++] = idleHorizontalTR[i][j];
-            }
-        }
-
-        Texture idleBackSheet = new Texture(Gdx.files.internal("./assets/sprites/robotBack" + colorModifier + ".png"));
-        TextureRegion[][] idleBackTR = TextureRegion.split(idleBackSheet,
-                idleBackSheet.getWidth() / BACK_FRAME_COLS,
-                idleBackSheet.getHeight() / BACK_FRAME_ROWS);
-        TextureRegion[] idleBackFrames = new TextureRegion[BACK_FRAME_COLS * BACK_FRAME_ROWS];
-        index = 0;
-        for (int i = 0; i < BACK_FRAME_ROWS; i++) {
-            for (int j = 0; j < BACK_FRAME_COLS; j++) {
-                idleBackFrames[index++] = idleBackTR[i][j];
-            }
-        }
-
-        Texture idleFrontSheet = new Texture(Gdx.files.internal("./assets/sprites/robotFront" + colorModifier + ".png"));
-        TextureRegion[][] idleFrontTR = TextureRegion.split(idleFrontSheet,
-                idleFrontSheet.getWidth() / FRONT_FRAME_COLS,
-                idleFrontSheet.getHeight() / FRONT_FRAME_ROWS);
-        TextureRegion[] idleFrontFrames = new TextureRegion[FRONT_FRAME_COLS * FRONT_FRAME_ROWS];
-        index = 0;
-        for (int i = 0; i < FRONT_FRAME_ROWS; i++) {
-            for (int j = 0; j < FRONT_FRAME_COLS; j++) {
-                idleFrontFrames[index++] = idleFrontTR[i][j];
-            }
-        }
-
+        color = COLORS[player.name()];
+        TextureRegion[] idleHorizontalFrames = getAnimationFrames("Side", HORIZONTAL_FRAME_ROWS, HORIZONTAL_FRAME_COLS);
+        TextureRegion[] idleBackFrames = getAnimationFrames("Back", BACK_FRAME_ROWS, BACK_FRAME_COLS);
+        TextureRegion[] idleFrontFrames = getAnimationFrames("Front", FRONT_FRAME_ROWS, FRONT_FRAME_COLS);
 
         idleHorizontalAnimation = new Animation<>(0.25f, idleHorizontalFrames);
         idleBackAnimation = new Animation<>(0.25f, idleBackFrames);
@@ -97,34 +64,44 @@ public class PlayerGraphic extends Image {
         setOrigin(Align.center);
     }
 
+    private TextureRegion[] getAnimationFrames(String orientation, int rows, int cols) {
+        Texture idleSheet = new Texture(Gdx.files.internal("./assets/sprites/robot" + orientation + color + ".png"));
+        TextureRegion[][] idleTR = TextureRegion.split(idleSheet, idleSheet.getWidth() / cols, idleSheet.getHeight() / rows);
+        TextureRegion[] frames = new TextureRegion[cols * rows];
+        int index = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                frames[index++] = idleTR[i][j];
+            }
+        }
+        return frames;
+    }
+
     public void updateAnimationFrame(float stateTime) {
         stateTime += timeModifier;
-
-        TextureRegion currentHorizontalFrame = idleHorizontalAnimation.getKeyFrame(stateTime, true);
-        TextureRegion currentBackFrame = idleBackAnimation.getKeyFrame(stateTime, true);
-        TextureRegion currentFrontFrame = idleFrontAnimation.getKeyFrame(stateTime, true);
+        TextureRegion currentFrame;
         if (player.getDirection() == Direction.EAST) {
-            if (currentHorizontalFrame.isFlipY()) {
-                currentHorizontalFrame.flip(false, true);
+            currentFrame = idleHorizontalAnimation.getKeyFrame(stateTime, true);
+            if (currentFrame.isFlipY()) {
+                currentFrame.flip(false, true);
             }
-            if (!currentHorizontalFrame.isFlipX()) {
-                currentHorizontalFrame.flip(true, false);
+            if (!currentFrame.isFlipX()) {
+                currentFrame.flip(true, false);
             }
-            setDrawable(new SpriteDrawable(new Sprite(currentHorizontalFrame)));
         } else if (player.getDirection() == Direction.WEST) {
-            if (currentHorizontalFrame.isFlipY()) {
-                currentHorizontalFrame.flip(false, true);
+            currentFrame = idleHorizontalAnimation.getKeyFrame(stateTime, true);
+            if (currentFrame.isFlipY()) {
+                currentFrame.flip(false, true);
             }
-            if (currentHorizontalFrame.isFlipX()) {
-                currentHorizontalFrame.flip(true, false);
+            if (currentFrame.isFlipX()) {
+                currentFrame.flip(true, false);
             }
-            setDrawable(new SpriteDrawable(new Sprite(currentHorizontalFrame)));
         } else if (player.getDirection() == Direction.NORTH) {
-            setDrawable(new SpriteDrawable(new Sprite(currentBackFrame)));
-        } else if (player.getDirection() == Direction.SOUTH) {
-            setDrawable(new SpriteDrawable(new Sprite(currentFrontFrame)));
+            currentFrame = idleBackAnimation.getKeyFrame(stateTime, true);
+        } else {
+            currentFrame = idleFrontAnimation.getKeyFrame(stateTime, true);
         }
-
+        setDrawable(new SpriteDrawable(new Sprite(currentFrame)));
     }
 
     public void animateMove() {
